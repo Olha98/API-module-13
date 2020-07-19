aca61a848dde737b9024cb90bcf8fe05990a5228
Olha Hrymchak <forwork1177@gmail.com>
2020-07-19 19:17:33 +0300

    add photify

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import './styles.css';
 import {
   refs
 } from './js/refs';
-import services from './js/services.js'
+import services from './js/services.js';
 import {
   updateAxiosMarkup
-} from './js/updateAxiosMarkup'
+} from './js/updateAxiosMarkup';
 import {
   buttonShowMore
-} from './js/buttonShowMore'
+} from './js/buttonShowMore';
 import basiclightbox from './js/basicLightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import 'basiclightbox/dist/basicLightbox.min.js';
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+PNotify.defaults.styling = 'bootstrap4';
 
 
 
@@ -29,17 +32,27 @@ refs.formSearch.addEventListener('submit', (e) => {
 
   services.axoisImage(search)
     .then(data => {
-      updateAxiosMarkup(data);
-      buttonShowMore.show();
-      window.scrollTo({
-        top: document.documentElement.offsetHeight,
-        behavior: 'smooth'
-      });
+      if (data.length > 0) {
+        PNotify.success('Hey BRO, good choice!')
+        updateAxiosMarkup(data);
+        buttonShowMore.show();
+        window.scrollTo({
+          top: document.documentElement.offsetHeight,
+          behavior: 'smooth'
+        });
+      } else {
+        PNotify.error(`We didn't find. Please, choise other theme of image...`)
+        return
+      }
+
+
     })
 });
 
-refs.ShowMore.addEventListener('click', () => {
+refs.ShowMore.addEventListener('click', (e) => {
+  e.preventDefault();
   services.axoisImage(search).then(data => {
+    PNotify.success('Wow, you want more!');
     updateAxiosMarkup(data);
   })
 })
--- src/js/services.js
+++ src/js/services.js
@@ -1,15 +1,16 @@
 const axios = require('axios').default;
 
+
 export default {
   page: 1,
   axoisImage(searchInput) {
     const API_key = '17505359-e5f9f93771b0e5762108364f3';
     return axios.get(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchInput}&page=${this.page}&per_page=12&key=${API_key}`)
-    .then(data => {
-      this.page += 1;
-      return (data.data.hits);
-    })
-      .catch(error => console.log(error));
+      .then(data => {
+        this.page += 1;
+        return (data.data.hits);
+      })
+      .catch(error => PNotify.error(`${error}`));
 
 
   },
